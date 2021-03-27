23334d03922ad65e383f68616173d19361b5f8bd
cjude62 <53706821+cjude62@users.noreply.github.com>
2021-03-27 22:56:55 +0300

    1st completion folders
    Images, styles and scripts

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/ecoworldlogonobg.png'){
+    myImage.setAttribute('src','images/curry.jpg');
+  } else {
+    myImage.setAttribute('src', 'images/stew.jpg');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name. ');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Kibet is so cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Kibet is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
